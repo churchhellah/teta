6ada9baf637aded12caee249f5b8511c37baf497
Dmitriy Shinkarev <DShinkarev@mtsbank.ru>
2024-11-19 17:53:22 +0300

    создан класс MessageValidator добавлена логика валидации JSON - проверяется фрмат сообщения и наличие в сообщении параметра msisdn

--- src/main/java/org/teta/MessageValidator.java
+++ src/main/java/org/teta/MessageValidator.java
@@ -1,4 +1,43 @@
 package org.teta;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import org.teta.dto.Message;
+
+/*
+    Сообщение должно быть в формате JSON.
+    В JSON должно быть поле msisdn со строковым значением. Остальные поля произвольны.
+    Если одно из условий не соблюдается (сообщение не формате JSON, поле msisdn отсутствует или информация не найдена),
+    сообщение возвращается в том же виде, в котором пришло.
+ */
+
 public class MessageValidator {
+
+    public Message validate(Message message) {
+        try {
+            // Попытка разобрать входящее сообщение, как JSON
+            JsonElement jsonElement = JsonParser.parseString(String.valueOf(message));
+
+            // Проверка, является ли jsonElement объектом
+            if (jsonElement.isJsonObject()) {
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+                // Проверка наличия поля msisdn
+                if (jsonObject.has("msisdn") &&
+                        jsonObject.get("msisdn").isJsonPrimitive() &&
+                        jsonObject.get("msisdn").getAsJsonPrimitive().isString() &&
+                        jsonObject.get("msisdn").getAsString().length() == 11) {
+                    // Валидация успешна - возвращаем исходное сообщение
+                    return message;
+                }
+            }
+        } catch (JsonSyntaxException e) {
+            // Если Message не является JSON - возвращаем исходное сообщение
+        }
+
+        // Возвращаем исходный Message, если валидация не пройдена
+        return message;
+    }
 }
